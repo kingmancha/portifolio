2aa779c83a6e56351fa118a26306c359cd489269
Kingmancha <mgabrielvr123@gmail.com>
2024-10-02 18:52:59 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulos'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/kingmancha.png" />
-    <Titulo fontSize={20}>Marcos Gabriel</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Marcos G.</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        kingmancha
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-End
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
