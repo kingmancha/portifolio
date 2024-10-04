2e9f705e2dec8b1acee3b5ee5fff5ab592adc2b6
Kingmancha <mgabrielvr123@gmail.com>
2024-10-04 16:04:55 -0300

    Alteracoes finais

--- src/containers/Sidebar/styles.ts
+++ src/containers/Sidebar/styles.ts
@@ -20,4 +20,9 @@ export const SidebarContainer = styled.div`
   position: sticky;
   top: 80px;
   left: 0;
+
+  @media (max-width: 768px) {
+    margin-bottom: 40px;
+    text-align: center;
+  }
 `
